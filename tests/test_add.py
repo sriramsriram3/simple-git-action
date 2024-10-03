9b19f9bb0cb598b9ac1477706074590da5493d64
simhadri <simhadrisriram3@gmail.com>
2024-10-03 11:35:38 +0530

    everything is up to date

---
+++ tests/test_add.py
@@ -0,0 +1,13 @@
+from src.math import sub,add
+
+def test_addexample():
+    assert add(2,3) == 5
+    assert add(4,6) == 10
+    assert add(0,0) == 0
+    assert add(-1,-1) == -2
+
+def test_subexample():
+    assert sub(3,2) == 1
+    assert sub(-1,-1) == 0
+    assert sub(2,-5) ==-31
+    assert sub(0,0) == 0
